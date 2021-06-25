415c47fa49e755f7ef38a5540eb0f1316feed824
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-06-25 13:06:38 -0300

    fix: replacing hard tabs with spaces

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/sign-up", async (req, res) => {
-	try {
-		const { name, password, email } = req.body;
-		const hashedPassword = bcrypt.hashSync(password, 10);
+  try {
+    const { name, password, email } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
     const existingUserResult = await connection.query(
       `SELECT * FROM users WHERE email = $1`,
@@ -31,108 +31,108 @@ app.post("/sign-up", async (req, res) => {
     } else {
       res.sendStatus(409);
     }
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.post("/sign-in", async (req, res) => {
-	try {
-		const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-		const result = await connection.query(
-			`SELECT * FROM users WHERE users.email = $1`,
-			[email]
-		);
+    const result = await connection.query(
+      `SELECT * FROM users WHERE users.email = $1`,
+      [email]
+    );
 
-		const user = result.rows[0];
+    const user = result.rows[0];
 
-		if (user && bcrypt.compareSync(password, user.password)) {
-			const token = uuid();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = uuid();
 
-			await connection.query(
-				`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`,
-				[user.id, token]
-			);
+      await connection.query(
+        `INSERT INTO sessions ("userId", token) VALUES ($1, $2)`,
+        [user.id, token]
+      );
 
-			res.send({ token });
-		} else {
-			res.sendStatus(401);
-		}
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+      res.send({ token });
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.post("/songs", async (req, res) => {
-	try {
-		const { title, plays } = req.body;
+  try {
+    const { title, plays } = req.body;
 
-		const authorization = req.headers["authorization"];
-		const token = authorization.replace("Bearer ", "");
+    const authorization = req.headers["authorization"];
+    const token = authorization.replace("Bearer ", "");
 
     const user = await connection.query(
-			`
-				SELECT * FROM sessions 
-				JOIN users ON users.id = sessions."userId"
-				WHERE sessions.token = $1
-			`,
-			[token]
-		);
+      `
+        SELECT * FROM sessions 
+        JOIN users ON users.id = sessions."userId"
+        WHERE sessions.token = $1
+      `,
+      [token]
+    );
 
     if (user.rows[0]) {
-			await connection.query(
+      await connection.query(
         `INSERT INTO songs (title, plays) VALUES ($1,$2)`,
         [title, plays]
       );
 
-			res.sendStatus(201);
-		} else {
-			res.sendStatus(401);
-		}
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.get("/songs", async (req, res) => {
-	try {
+  try {
     const result = await connection.query(`SELECT * FROM songs`);
     res.send(result.rows);
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 // usuario marca musica como favorita
 app.post("/songs/:songId/bookmark", async (req, res) => {
-	try {
-		const { songId } = req.params;
+  try {
+    const { songId } = req.params;
 
-		const music = await connection.query(
-			`SELECT * FROM songs WHERE id = $1`,
-			[songId]
-		);
+    const music = await connection.query(
+      `SELECT * FROM songs WHERE id = $1`,
+      [songId]
+    );
 
-		if (!music.rows[0]) {
-			return res.sendStatus(404);
-		}
+    if (!music.rows[0]) {
+      return res.sendStatus(404);
+    }
 
     const authorization = req.headers["authorization"];
-		const token = authorization.replace("Bearer ", "");
+    const token = authorization.replace("Bearer ", "");
 
     const user = await connection.query(
-			`
-				SELECT * FROM sessions 
-				JOIN users ON users.id = sessions."userId"
-				WHERE sessions.token = $1
-			`,
-			[token]
-		);
+      `
+        SELECT * FROM sessions 
+        JOIN users ON users.id = sessions."userId"
+        WHERE sessions.token = $1
+      `,
+      [token]
+    );
 
     if (user.rows[0]) {
       const userId = user.rows[0].id;
@@ -146,32 +146,32 @@ app.post("/songs/:songId/bookmark", async (req, res) => {
     } else {
       res.sendStatus(401);
     }
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 // pega os usuarios e suas musicas favoritas.
 app.get("/users/songs", async (req, res) => {
-	try {
-		const result = await connection.query(`
-			SELECT users.*, songs.title AS "songName" FROM users
-			JOIN "songsUsers" ON users.id = "songsUsers"."userId"
-			JOIN songs ON songs.id = "songsUsers"."songId"   
-		`);
+  try {
+    const result = await connection.query(`
+      SELECT users.*, songs.title AS "songName" FROM users
+      JOIN "songsUsers" ON users.id = "songsUsers"."userId"
+      JOIN songs ON songs.id = "songsUsers"."songId"   
+    `);
 
-		res.send(result.rows);
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+    res.send(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 // pega as musicas favoritas de um usuario
 app.get("/users/:userId/songs", async (req, res) => {
-	try {
-		const { userId } = req.params;
+  try {
+    const { userId } = req.params;
 
     const existingUser = await connection.query(
       `SELECT * FROM users WHERE id = $1`,
@@ -193,10 +193,10 @@ app.get("/users/:userId/songs", async (req, res) => {
 
       res.send(result.rows);
     }
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 export default app;
